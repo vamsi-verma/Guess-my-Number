ac21e4cd5a1200ef6eb0e2684acc273bb0453348
Vamsi Verma <vamsiverma1998@gmail.com>
2022-01-24 08:54:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Guess-my-Number
+ This mini Poject is a game where a user has to guess a number between 1 and 20 and if his guess is correct , the umber of chances used out of 20 to get that number will be his/her score
---
+++ script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    displayMessage('⛔ No number!');
+  } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈Too high' : '📉Too low');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥You lost the game');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+//reset button
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+});
